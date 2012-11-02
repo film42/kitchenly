3ae83b0bd880feec0a6848f713256017d5fb625d
Byron <behundley@gmail.com>
2012-11-02 15:25:11 -0600

    added routes for search and user

--- webapp/app.js
+++ webapp/app.js
@@ -32,6 +32,8 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/results', routes.searchResults);
+app.get('/user', routes.user);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -12,3 +12,27 @@ exports.index = function(req, res){
 
   });
 };
+
+exports.index = function(req, res) {
+  	res.render('index', { title: "hello" });
+};
+
+// Get the search results page.
+exports.searchResults = function(req, res) {
+    couchdb.getAllUsers(function(users) {
+        //var users = { };
+        var viewData = { title: 'Search results' };
+        viewData.users = users;
+        res.render('results', viewData);
+    });
+};
+
+// Get the user details page.
+exports.user = function(req, res) {
+    couchdb.getUserById(req.userId, true, function(user) {
+        //var user = { name: {first_name:"Byron", last_name: "Hundley"}, photo_url: "//lh4.googleusercontent.com/-gP3mcTlmkFQ/AAAAAAAAAAI/AAAAAAAAAAA/aSzXUf-kBk0/s27-c/photo.jpg"};
+        var viewData = { title: 'User' };
+        viewData.user = user;
+        res.render('userInfo', viewData);
+    });
+};
