21e8cb2e46f130eb89613a0448ed3520f643ec51
film42 <film42@gmail.com>
2012-11-02 10:47:11 -0600

    Db so far

--- webapp/custom_modules/dbInterface.js
+++ webapp/custom_modules/dbInterface.js
@@ -11,16 +11,29 @@ var db = nano.use('kitchenly');
 /*************************
     User interfaces
 *************************/
-exports.getAllUsers = function(cb) {
+exports.getAllUsers = function(inflated, cb) {
     db.view("users", 'all', function(err, docs) {
-        if(err) throw new Error("getAllUsers " + err)
-        cb(docs);
+        if(err) throw new Error("getAllUsers " + err) {
+            for(object in docs.rows) {
+                // Get Reviews
+                var reviews;
+                for(review in object.reviews) {
+                    reviews.push(exports.getReviewById);
+                }
+                object.reviews = reviews;
+
+                var venues;
+                // Venues
+            }
+        }
     });
 } // _design/users/_view/all
-exports.getUserById = function(id) {
+exports.getUserById = function(id, inflated, cb) {
     db.view("users", 'all', {key: id}, function(err, doc) {
         if(err) throw new Error("getUserById " + err)
-        cb(doc);
+            // Get Reviews
+            // Venues
+            cb(doc);
     });
 } // _design/users/_view/all?id=###
 
