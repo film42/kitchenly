7c3eaed7a9d844b07b3e5c84f3b9585727385dbf
kimballfrank <kimballfrank@hotmail.com>
2012-11-03 12:14:07 +0100

    login form started, and continued work on the search results page

--- webapp/public/scripts/onload.js
+++ webapp/public/scripts/onload.js
@@ -19,10 +19,8 @@ $(document).ready(function() {
       $('#guest-number-picker .dropdown-toggle span.num').html($(this).html());
     });
 
-    
-    
-
-// list of cities for typeahead feature 		
+  // list of cities for typeahead feature 		
   		var cityList = ['Baltimore, MD', 'Boston, MA', 'New York, NY', 'Tampa Bay, FL', 'Chicago', 'Cleveland, OH', 'Detroit, MI', 'Kansas City, MO', 'Los Angeles, CA', 'Oakland, CA', 'Seattle, WA', 'Houston, TX ', 'Provo, UT', 'Salt Lake City, UT', 'Ogden, UT', 'Orem, UT', 'Sandy, UT', 'West Jordan, UT', 'Saint George, UT'].sort();
   		$('#location').typeahead({source: cityList, items:5});
+  		
 });
