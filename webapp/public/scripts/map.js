266d5b3491b122b074b425ddb666146a7a3d10e7
kimballfrank <kimballfrank@hotmail.com>
2012-11-03 20:36:40 +0100

    added google map to search page. still needs data from db or otherwise.

--- webapp/public/scripts/map.js
+++ webapp/public/scripts/map.js
@@ -1,58 +1,54 @@
-﻿<!DOCTYPE html>
- <html>
-     <body>
-      
-      <div id="map_canvas" style="width:425px; height:550px"></div> 
+﻿$(document).ready(function() {
 
-      <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script> 
-      <script type="text/javascript"> 
+  var map;
+  var markers = [];
+  var geocoder;
+  
+ $('#map-canvas').attr('style','height:200px; margin-bottom:28px;');
+  
+  initialize();
+  
+  function initialize() {
+  	geocoder = new google.maps.Geocoder();
+  
+  	var myOptions = {
+  		zoom: 10,
+  		mapTypeId: google.maps.MapTypeId.ROADMAP
+  	}
+  	map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+  	codeAddress("84403");
+  	addMarker("253 25th Street, Ogden, UT", "Roosters Brewing");
+  	addMarker("2501 Wall Ave, Ogden, UT", "Union Grill");
+  	addMarker("195 25th Street, Ogden, UT", "jasoh!");
+  }
+  
+  function codeAddress(address) {
+  	geocoder.geocode({
+  		'address': address
+  	}, function(results, status) {
+  		if (status == google.maps.GeocoderStatus.OK) {
+  			map.setCenter(results[0].geometry.location);
+  		} else {
+  			alert("Geocode was not successful for the following reason: " + status);
+  		}
+  	});
+  
+  }
+  
+  function addMarker(address, myTitle) {
+  	geocoder.geocode({
+  		'address': address
+  	}, function(results, status) {
+  		if (status == google.maps.GeocoderStatus.OK) {
+  			var marker = new google.maps.Marker({
+  				map: map,
+  				title: myTitle,
+  				position: results[0].geometry.location
+  			});
+  		} else {
+  			alert("Geocode was not successful for the following reason: " + status);
+  		}
+  	});
+  }
 
-        var map;
-        var markers = [];
-		var geocoder;
-        var map;
-
-        initialize();
-
-        function initialize() {
-			geocoder = new google.maps.Geocoder();
-			
-			var myOptions = {
-				zoom: 10,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			}
-			map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-			codeAddress("84403");
-			addMarker("253 25th Street, Ogden, UT", "Roosters Brewing");
-            addMarker("2501 Wall Ave, Ogden, UT","Union Grill");
-            addMarker("195 25th Street, Ogden, UT", "jasoh!");
-        }
-
-		function codeAddress(address) {
-			geocoder.geocode( { 'address': address}, function(results, status) {
-			  if (status == google.maps.GeocoderStatus.OK) {
-				map.setCenter(results[0].geometry.location);
-			  } else {
-				alert("Geocode was not successful for the following reason: " + status);
-			  }
-			});
-			
-		  }
-
-        function addMarker(address, myTitle) {
-          geocoder.geocode( { 'address': address}, function(results, status) {
-			  if (status == google.maps.GeocoderStatus.OK) {
-				var marker = new google.maps.Marker({
-					map: map,
-					title: myTitle,
-					position: results[0].geometry.location
-				});
-			  } else {
-				alert("Geocode was not successful for the following reason: " + status);
-			  }
-			});    
-        }
-
-      </script> 
-    </body>
-  </html>
+});
