208c7b7c573058c69e2f1309bf5e12f49e74760f
kimballfrank <kimballfrank@hotmail.com>
2012-11-03 09:28:05 +0100

    autocomplete junk finished for demo

--- webapp/public/scripts/onload.js
+++ webapp/public/scripts/onload.js
@@ -17,6 +17,6 @@ $(document).ready(function() {
 //  		    }
 //  		});
   		
-  		var allCities = ['Baltimore, MD', 'Boston, MA', 'New York, NY', 'Tampa Bay, FL', 'Chicago', 'Cleveland, OH', 'Detroit, MI', 'Kansas City, MO', 'Los Angeles, CA', 'Oakland, CA', 'Seattle, WA', 'Houston, TX ', 'Provo, UT', 'Salt Lake City, UT', 'Ogden, UT', 'Orem, UT', 'Sandy, UT', 'West Jordan, UT', 'Orem, UT', 'Saint George, UT'].sort();
-  		$('#location').typeahead({source: allCities, items:8});
+  		var cityList = ['Baltimore, MD', 'Boston, MA', 'New York, NY', 'Tampa Bay, FL', 'Chicago', 'Cleveland, OH', 'Detroit, MI', 'Kansas City, MO', 'Los Angeles, CA', 'Oakland, CA', 'Seattle, WA', 'Houston, TX ', 'Provo, UT', 'Salt Lake City, UT', 'Ogden, UT', 'Orem, UT', 'Sandy, UT', 'West Jordan, UT', 'Saint George, UT'].sort();
+  		$('#location').typeahead({source: cityList, items:5});
 });
