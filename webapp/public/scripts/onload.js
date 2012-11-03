83caff43cffc01ef0d20ebb16ec2f7fe8aafaafe
kimballfrank <kimballfrank@hotmail.com>
2012-11-03 11:07:38 +0100

    revert this

--- webapp/public/scripts/onload.js
+++ webapp/public/scripts/onload.js
@@ -1,22 +1,28 @@
 $(document).ready(function() {
-      $('#calendar-picker').hide();
-      
-      $('#date-time').focusin( function(){
-        $('#calendar-picker').show();
-      });
-      
-//      $('#date-time').focusout( function(){
-//        $('#calendar-picker').hide();
-//      });
-      		
-//  		$('.typeahead').typeahead({
-//  		    source: function (query, process) {
-//  		        return $.get('http://gd.geobytes.com/AutoCompleteCity?callback=?&q=',{query: query}, function (data) {
-//  		            return process(data.options);
-//  		        });
-//  		    }
-//  		});
-  		
+
+    var calHTML = $('#calendar-picker-contain').html();
+    $('#calendar-picker-contain').remove();
+    
+    $('#date-time').popover({
+      html: true,
+      content: calHTML
+    });
+    
+    $('#date-time').blur( function(){
+      $('#date-time').popover('hide');
+    });
+    
+    
+    $('#guest-number-picker .dropdown-menu li a').click( function(){
+      $('#guest-number-picker .dropdown-menu li a').removeClass('active');
+      $(this).addClass('active');
+      $('#guest-number-picker .dropdown-toggle span.num').html($(this).html());
+    });
+
+    
+    
+
+// list of cities for typeahead feature 		
   		var cityList = ['Baltimore, MD', 'Boston, MA', 'New York, NY', 'Tampa Bay, FL', 'Chicago', 'Cleveland, OH', 'Detroit, MI', 'Kansas City, MO', 'Los Angeles, CA', 'Oakland, CA', 'Seattle, WA', 'Houston, TX ', 'Provo, UT', 'Salt Lake City, UT', 'Ogden, UT', 'Orem, UT', 'Sandy, UT', 'West Jordan, UT', 'Saint George, UT'].sort();
   		$('#location').typeahead({source: cityList, items:5});
 });
